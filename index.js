dd91ac579bb3a0b7abbf13d0631fe2101d838e3a
DiKeySnakes <DiKeySnakes@gmail.com>
2023-01-07 17:37:43 +0200

    Add index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const merge = (left, right) => {
+  let sortedArr = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+  return [...sortedArr, ...left, ...right];
+};
+
+const mergeSort = (arr) => {
+  if (arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length / 2);
+  let left = mergeSort(arr.slice(0, mid));
+  let right = mergeSort(arr.slice(mid));
+  return merge(left, right);
+};
+
+console.log(mergeSort([98, 57, 18, 45, 90, 99, 2002, 357]));
